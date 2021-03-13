95661053011e10f4592106692cf5a7b06a292c2e
dedelita <hello.dedel@gmail.com>
2021-03-13 15:31:31 +0100

    Passage de ViewPager à ViewPager2

--- app/src/main/java/com/example/keskonmange/main/MainActivity.kt
+++ app/src/main/java/com/example/keskonmange/main/MainActivity.kt
@@ -2,28 +2,39 @@ package com.example.keskonmange.main
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.example.keskonmange.R
 import com.example.keskonmange.fragment.BarFragment
 import com.example.keskonmange.fragment.MaisonFragment
 import com.example.keskonmange.fragment.NewRestoDialogFragment
 import com.example.keskonmange.adapter.SectionsPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(), BarFragment.OnListFragmentInteractionListener, MaisonFragment.OnListFragmentInteractionListener, NewRestoDialogFragment.OnFragmentInteractionListener, NewRestoDialogFragment.DialogListener {
 
+    private val TAB_TITLES = listOf(
+        R.string.tab_text_2,
+        R.string.tab_text_1
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = getString(TAB_TITLES[position])
+        }.attach()
     }
 
     override fun onListFragmentInteraction(item: Resto?) {
